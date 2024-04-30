e1943a54a39aa2e54139735822b8364c6a83bdfd
bquochtruong <brianquochungtruong@gmail.com>
2024-04-29 19:45:11 -0700

    Add transition

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Document loaded');
 });
+window.onload = function() {
+    document.body.style.opacity = '1'; // Set opacity to 1 when page loads
+};
