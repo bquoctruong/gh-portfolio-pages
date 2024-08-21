14928dbd0ac319f7c3084124c0105f760c8ea3f4
bquochtruong <brianquochungtruong@gmail.com>
2024-08-21 13:11:57 -0700

    Updated directory; fall cleaning

--- js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Document loaded');
-});
-window.onload = function() {
-    document.body.style.opacity = '1'; // Set opacity to 1 when page loads
-};
